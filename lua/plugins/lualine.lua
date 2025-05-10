007557d1994c842cf1e4f5c8b9b1e07f2903e763
jades-not-developing <jadesnotdeveloping@gmail.com>
2025-05-10 10:26:32 -0400

    add nvim-autopairs + config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,6 +6,21 @@ return {
         },
         config = function()
             require("lualine").setup({
+                options = {
+                    section_separators = { left = '', right = '' },
+                },
+                sections = {
+                    lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+                    lualine_b = { 'filename', 'branch' },
+                    lualine_c = {
+                        '%=', --[[ add your center components here in place of this comment ]]
+                    },
+                    lualine_x = {},
+                    lualine_y = { 'filetype', 'progress' },
+                    lualine_z = {
+                        { 'location', separator = { right = '' }, left_padding = 2 },
+                    },
+                },
                 theme = "dracula",
             })
         end
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local ts_config = require("nvim-treesitter.configs")
         ts_config.setup({
-            ensure_installed = {"lua", "zig", "rust", "typescript", "javascript"},
+            ensure_installed = {"lua", "zig", "rust", "typescript", "javascript", "asm"},
             highlight = { enable = true },
             indent = { enable = true },
         })
